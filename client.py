78fc2de8074ebf0fffdeb46cbdfa3bf76a74debd
moussa <adjimahamat@gmail.com>
2020-03-31 00:37:55 +0200

    Socket: Reverse shell | MSG works | QUIT works but some bugs

--- client.py
+++ client.py
@@ -1,15 +1,53 @@
-import socket
+import socket, select, errno, sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-s.connect((socket.gethostname(), 1234));
+IP = "127.0.0.1";
+PORT = 1234;
+HEADER_LENGTH = 1024;
+LOGOUT = "logout";
+my_username = input("Username: ");
 
-buff = 2;
-char = s.recv(buff);
-msg = char;
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+client_socket.connect((IP, PORT));
+client_socket.setblocking(False);
+
+username = my_username.encode("utf-8");
+username_header = f'{len(username):<{HEADER_LENGTH}}'.encode('utf-8');
+client_socket.send(username_header + username);
+
 
 while True:
-	char = s.recv(buff);
-	msg = msg + char;
-	if len(char) == 1:
-		break;
-print(msg.decode("utf-8"));
+	message = input(f"{my_username}@client > ");
+	# message = "";				
+	
+	if message:
+		message = message.encode('utf-8');
+		message_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8");
+		client_socket.send(message_header + message);
+	try:
+		while True:
+			#receive things here
+			username_header = client_socket.recv(HEADER_LENGTH);
+			if not len(username_header):
+				print("Connection closed by the server");
+				sys.exit();
+
+			username_length = int(username_header.decode("utf-8").strip());
+			username = client_socket.recv(username_length).decode("utf-8");
+
+			message_header = client_socket.recv(HEADER_LENGTH);
+			message = client_socket.recv(HEADER_LENGTH).decode("utf-8");
+
+			print(f"{username}@client > {message}");
+			if message == LOGOUT:
+				client_socket.close();	
+				sys.exit();
+
+	except IOError as e:
+		if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+			print(f"Reading error {str(e)}");
+			sys.exit();
+		continue;
+
+	except Exception as e:
+		print(f"General error {str(e)}");
+		sys.exit();
--- server.py
+++ server.py
@@ -1,11 +1,91 @@
-import socket
+import socket, select
 
+# ------------------------------------------------------------------------------ #
+# -------------------------------- Constants Socket ---------------------------- #
+IP = "127.0.0.1";
+PORT = 1234;
+HEADER_LENGTH = 1024;
+CMD_DELIMITOR = " ";
+# ------------------------------------------------------------------------------ #
+
+
+# -------------------------------- Constants command--------------------------- #
+CMD_QUIT = "QUIT";
+MSG_QUIT = "Leaved"
+CMD_MSG = "MSG";
+
+CMD_NICK = "NICK";
+CMD_KILL = "KILL";
+MSG_KIL = "Stopped"
+# ------------------------------------------------------------------------------ #
+# -------------------------------- Init ---------------------------------------- #
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-s.bind((socket.gethostname(), 1234));
-s.listen(5);
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1);
+s.bind((IP, PORT));
+s.listen(HEADER_LENGTH);
+s_list = [s];
+clients = {};
+
+# ------------------------------------------------------------------------------ #
+
+
+def receive_message(client_socket):
+	try:
+		message_hedaer = client_socket.recv(HEADER_LENGTH);
+		if not len(message_hedaer):
+			return False;
+		message_length = int(message_hedaer.decode("utf-8").strip());
+		return {"header": message_hedaer, "data": client_socket.recv(HEADER_LENGTH)};
+	except:
+		return False;
+
 
 while True:
-	clientsocket, address = s.accept();
-	print "Connection from {} has been established!"format(address);
-	clientsocket.send(bytes("Welcome to the server !"));
-	clientsocket.close();
+	read_sockets, _, exception_sockets = select.select(s_list, [], s_list);
+	for notified_socket in read_sockets:
+		if notified_socket == s:
+			client_socket, client_address = s.accept();
+			user = receive_message(client_socket);
+			if user is False:
+				continue;
+			s_list.append(client_socket);
+			clients[client_socket] = user;
+			print(f"Accepted new connection {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}");
+		else:
+			message = receive_message(notified_socket);
+			if message is False:
+				print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}");
+				s_list.remove(notified_socket);
+				del clients[notified_socket];
+				continue;
+			user = clients[notified_socket];
+			data = message['data'].decode('utf-8');
+			cmd_line = data.split(CMD_DELIMITOR);
+			type_cmd = cmd_line[0];
+			msg = "";
+			if type_cmd == CMD_QUIT :
+				msg = MSG_QUIT;
+				if len(cmd_line) == 2 :
+					msg += " by saying : " + cmd_line[1];
+				print(f"Received message from {user['data'].decode('utf-8')} : {data}");
+				for c_socket in clients:
+					if c_socket != notified_socket:
+						c_socket.send(user['header']+user['data']+message['header']+msg.encode('utf-8'));
+
+				client_socket.send(user['header']+user['data']+message['header']+"logout".encode('utf-8'));
+				client_socket.close();
+				s_list.remove(notified_socket);
+				del clients[notified_socket];
+
+			if type_cmd == CMD_MSG:
+				if len(cmd_line) == 2 :
+					msg = cmd_line[1];
+					print(f"Received message from {user['data'].decode('utf-8')} : {data}");
+
+				for c_socket in clients:
+					if c_socket != notified_socket:
+						c_socket.send(user['header']+user['data']+message['header']+msg.encode('utf-8'));
+
+	for notified_socket in exception_sockets:
+		s_list.remove(notified_socket);
+		del clients[notified_socket];
